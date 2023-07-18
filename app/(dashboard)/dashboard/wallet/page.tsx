445d3626e2abb3f0c39706496b40961243b283bd
krendus <xaiyax01@gmail.com>
2023-07-18 22:51:15 +0100

    Added wallet page

---
+++ app/(dashboard)/dashboard/wallet/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Wallet from './wallet'
+
+const Page = () => {
+  return (
+    <><Wallet /></>
+  )
+}
+export const metadata = {
+    title: "BM DAO | Wallet"
+}
+
+export default Page
