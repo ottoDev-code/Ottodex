69347f84ca5c55ad39815cf7d18b44a962c2f3d6
krendus <xaiyax01@gmail.com>
2023-07-29 16:59:00 +0100

    fix

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Dashboard from './dashboard';
+import ClientDashboard from './client-dashboard';
 
 const Page = () => {
   return (
     <><Dashboard /></>
+    // <><ClientDashboard /></>
   )
 }
 
