4c8578ee32ee55dc79fe4068013b42973930c947
krendus <xaiyax01@gmail.com>
2023-08-06 09:04:59 +0100

    fixes

---
+++ app/components/dashbord-nav/dashboard-nav.tsx
@@ -0,0 +1,135 @@
+"use client"
+import Image from 'next/image'
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
+import { Auth, Container, Dropdown, Links, Logo, MAuth, MLinks, NavButton, Wrapper } from './style'
+import { useRouter, usePathname } from 'next/navigation'
+import { SubNavButton, SubNavWrapper } from '../sidebar/style'
+import { ArrowDownIcon, BookIcon, DocumentIcon, HomeIcon, LogoutIcon, UserIcon, UsersIcon, WalletIcon } from '../svg-icons'
+
+const Nav = () => {
+  const path = usePathname();
+  const [scrolled, setScrolled] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [show, setShow] = useState(false);
+  const pathname = usePathname();
+  const router = useRouter();
+
+
+  // Track if the page is scrolled then the styling of the nav is updated
+  const handleScroll = (e: Event) => {
+    if((document.body.scrollTop > 100) || document.documentElement.scrollTop > 100) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  }
+  const handleRouting = (route: string) => {
+    router.push(route);
+    setShowDropdown(false);
+  }
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    }
+  }, [])
+  
+  return (
+    <Container $isScrolled={scrolled}>
+      <Wrapper>
+        <Logo>
+          <Image src={"/logo.svg"} alt="logo" height={50} width={148} />
+        </Logo>
+        <button className='nav-btn' onClick={() => setShowDropdown(!showDropdown)}>
+          <Image src={showDropdown ? "/close-icon.svg" : "/menu.svg"} alt="logo" height={36} width={36} />
+        </button>
+      </Wrapper>
+      {
+        showDropdown ? (
+          <Dropdown>
+            <MLinks>
+              <NavButton $isActive={pathname === "/dashboard"} onClick={() => handleRouting("/dashboard")}>
+                  <div>
+                    <HomeIcon />
+                    <span>Dashboard</span>
+                  </div>
+              </NavButton>
+              <NavButton onClick={() => setShow(!show)} $isActive={pathname.includes("/dashboard/tasks")}>
+                  <div>
+                    <DocumentIcon />
+                    <span>Tasks</span>
+                  </div>
+                  <ArrowDownIcon />
+              </NavButton>
+              {
+                show ? (
+                  <SubNavWrapper>
+                    <SubNavButton onClick={() => handleRouting("/dashboard/tasks/moderators")}>
+                      <div>
+                        <Image src="/mapper.svg" alt="" height={50} width={24}/>
+                      </div>
+                      <span>Moderators</span>
+                    </SubNavButton>
+                    <SubNavButton onClick={() => handleRouting("/dashboard/tasks/collab-managers")}>
+                      <div>
+                        <Image src="/mapper.svg" alt="" height={50} width={24}/>
+                      </div>
+                      <span>Collab Managers</span>
+                    </SubNavButton>
+                    <SubNavButton onClick={() => handleRouting("/dashboard/tasks/twitter-raiders")}>
+                      <div>
+                        <Image src="/mapper.svg" alt="" height={50} width={24}/>
+                      </div>
+                      <span>Twitter Raiders</span>
+                    </SubNavButton>
+                    <SubNavButton  onClick={() => handleRouting("/dashboard/tasks/chat-engagers")}>
+                      <div>
+                        <Image src="/mapper.svg" alt="" height={50} width={24}/>
+                      </div>
+                      <span>Chat Engagers</span>
+                    </SubNavButton>
+                  </SubNavWrapper>
+                ) : null
+              }
+              <NavButton $isActive={pathname === ("/dashboard/wallet")} onClick={() => handleRouting("/dashboard/wallet")}>
+                  <div>
+                    <WalletIcon />
+                    <span>Wallet</span>
+                  </div>
+              </NavButton>
+              <NavButton $isActive={pathname === "/dashboard/profile"}  onClick={() => handleRouting("/dashboard/profile")}>
+                  <div>
+                    <UserIcon />
+                    <span>Profile</span>
+                  </div>
+              </NavButton>
+              <NavButton $isActive={pathname === "/dashboard/academy"}  onClick={() => handleRouting("/dashboard/academy")}>
+                  <div>
+                    <BookIcon />
+                    <span>Academy</span>
+                  </div>
+              </NavButton>
+              <NavButton $isActive={pathname === "/dashboard/referral"}  onClick={() => handleRouting("/dashboard/referral")}>
+                  <div>
+                    <UsersIcon />
+                    <span>Referral</span>
+                  </div>
+              </NavButton>
+            </MLinks>
+            <MAuth>
+              <NavButton>
+                  <div>
+                    <LogoutIcon />
+                    <span>Logout</span>
+                  </div>
+              </NavButton>
+            </MAuth>
+          </Dropdown>
+        ) : null
+      }
+    </Container>
+  )
+}
+
+export default Nav
---
+++ app/components/dashbord-nav/style.ts
@@ -0,0 +1,157 @@
+import theme from "@/app/styles/theme";
+import Link from "next/link";
+import { styled } from "styled-components";
+interface INavProps {
+    $isScrolled: boolean;
+}
+interface ILink {
+    $isActive?: boolean;
+}
+
+const { sizes, colors, breakpoints } = theme;
+
+export const Container = styled.div<INavProps>`
+    height: 80px;
+    width: 100%;
+    position: fixed;
+    top: 0;
+    display: none;
+    align-items: center;
+    justify-content: center;
+    background: ${({$isScrolled}) => $isScrolled ? colors.bgGrey : "transparent"};
+    z-index: 999999;
+    ${breakpoints.lg} {
+        background: ${colors.bgGrey};
+        display: flex;
+    }
+`;
+export const Wrapper = styled.div`
+    width: 92%;
+    max-width: ${sizes.wrapperWidth};
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .nav-btn {
+        background: transparent;
+        border: none;
+        outline: none;
+        display: none;
+        cursor: pointer;
+        ${breakpoints.lg} {
+            display: block;
+        }
+    }
+`;
+export const Logo = styled.div`
+`;
+export const Links = styled.div`
+    display: flex;
+    column-gap: 20px;
+    font-size: 16px;
+    a {
+        font-size: 16px;
+        font-weight: 500;
+    }
+    a:hover {
+        color: ${colors.black};
+    }
+    ${breakpoints.lg} {
+        display: none;
+    }
+`;
+export const NavButton = styled.button<ILink>`
+    padding: 10px;
+    background: transparent;
+    border: none;
+    outline: none;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    color: ${({ $isActive }) => $isActive ? "#000" : "#818181"};
+    justify-content: space-between;
+    border-radius: 5px;
+    &:hover {
+        color: #000;
+        background: ${colors.primaryColor};
+    }
+    div {
+        display: flex;
+        align-items: center;
+        column-gap: 10px;
+        font-size: 16px;
+        font-weight: 500;
+    }
+`;
+export const Auth = styled.div`
+    display: flex;
+    column-gap: 20px;
+    a {
+        font-weight: 500;
+        border: 1px solid ${colors.primaryColor};
+        border-radius: 5px;
+        padding: 5px 10px;
+        display: inline-block;
+    }
+    a:first-of-type {
+        color: ${colors.primaryColor};
+    }
+    a:last-of-type {
+        background: ${colors.primaryColor};
+        color: ${colors.black};
+    }
+    ${breakpoints.lg} {
+        display: none;
+    }
+`;
+export const Dropdown = styled.div`
+    display: none;
+    width: 100%;
+    position: absolute;
+    top: 80px;
+    left: 0;
+    background: ${colors.bgGrey};
+    height: calc(100vh - 80px);
+    min-height: calc(100vh - 80px);
+    flex-direction: column;
+    justify-content: space-between;
+    text-align: center;
+    padding: 20px 54px;
+    overflow-y: scroll;
+    ${breakpoints.lg} {
+        display: flex;
+    }
+`;
+export const MLinks = styled.div`
+    display: flex;
+    flex-direction: column;
+    font-size: 16px;
+    row-gap: 4px;
+    a {
+        font-size: 16px;
+        font-weight: 500;
+    }
+    a:hover {
+        color: ${colors.black};
+    }
+`;
+export const MAuth = styled.div`
+    display: flex;
+    row-gap: 20px;
+    flex-direction: column;
+    width: 100%;
+    a {
+        font-weight: 500;
+        border: 1px solid ${colors.primaryColor};
+        border-radius: 5px;
+        padding: 8px 10px;
+        width: 100%;
+        display: block;
+    }
+    a:first-of-type {
+        color: ${colors.primaryColor};
+    }
+    a:last-of-type {
+        background: ${colors.primaryColor};
+        color: ${colors.black};
+    }
+`;
